bbc687afe78e97622dda0cfa91e4842b75417239
Weslley Alberto <weslleyalberto@gmail.com>
2024-12-07 13:33:47 -0300

    Adicionado Contexto a controller

--- Back/src/ProEventos.API/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/ProEventos.API/Controllers/EventoController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -7,46 +9,22 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        private readonly Evento[] _evento =
-        [
-            new Evento(){
-                EventoId = 1,
-                Local = "Belo Horizonte",
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                Tema = "Angular 11 e .NET 5",
-                QtdPessoas = 250,
-                Lote = "1º Lote",
-                ImageURL = "foto.jpg"
-            },
-            new Evento(){
-                EventoId = 2,
-                Local = "São Paulo",
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                Tema = "Angular 11 e .NET 5",
-                QtdPessoas = 250,
-                Lote = "1º Lote",
-                ImageURL = "foto2.jpg"
-            },
-            new Evento(){
-                EventoId = 3,
-                Local = "Rio de Janeiro",
-                DataEvento = DateTime.Now.AddDays(4).ToString("dd/MM/yyyy"),
-                Tema = "Angular 11 e .NET 5",
-                QtdPessoas = 250,
-                Lote = "1º Lote",
-                ImageURL = "foto4.jpg"
-            }
-        ];
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
+        {
+            _context = context;
+        }
+       
 
         [HttpGet(Name = "GetEvento")]
-        public IEnumerable<Evento> Get()
+        public async Task<IEnumerable<Evento>> Get()
         {
-           return  _evento;
+           return  await _context.Eventos.AsNoTracking().ToListAsync();
         }
         [HttpGet("{id:int}", Name = "GetEventoById")]
-        public Evento? GetById(int id)
+        public async  Task<Evento> GetById(int id)
         {
-            return _evento.FirstOrDefault(evento => evento.EventoId == id);
+            return await _context.Eventos.AsNoTracking().FirstOrDefaultAsync(x => x.EventoId == id);
         }
     }
 }
