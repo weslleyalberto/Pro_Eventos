a9ea51bf6ad10ff862bda2daf34a098fe54494f8
Weslley Alberto <weslleyalberto@gmail.com>
2024-12-07 12:54:51 -0300

    criado Contexto

--- Back/src/ProEventos.API/ProEventos.API/Program.cs
+++ Back/src/ProEventos.API/ProEventos.API/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options => {
+    options.UseSqlite("Data Source=ProEventos.db");
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
