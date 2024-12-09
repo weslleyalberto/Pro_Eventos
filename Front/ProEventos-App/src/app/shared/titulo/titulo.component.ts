71e72f5cf701684103caf85b3531dd9879c2a51f
Weslley Alberto <weslleyalberto@gmail.com>
2024-12-09 19:53:36 -0300

    Implementado temas com Boostwatch

--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -7,6 +7,9 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TituloComponent implements OnInit {
    @Input() titulo!:string ;
+   @Input() iconClass:string = 'fa fa-user' ;
+   @Input() subtitulo:string  = "Desde 2024";
+   @Input() botaoListar:boolean = false;
   constructor() { }
 
   ngOnInit() {
