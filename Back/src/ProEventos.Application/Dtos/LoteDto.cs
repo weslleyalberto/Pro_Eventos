6876092d3f421874d2b342b72d42892caac70c10
Weslley Alberto <weslleyalberto@gmail.com>
2025-01-03 19:07:02 -0300

    Implemetado controller de Lotes e testado o CRUD

--- Back/src/ProEventos.Application/Dtos/LoteDto.cs
+++ Back/src/ProEventos.Application/Dtos/LoteDto.cs
@@ -11,6 +11,6 @@ namespace ProEventos.Application.Dtos
         public string DataFim { get; set; }
         public int Quantidade { get; set; }
         public int EventoId { get; set; }
-        public EventoDto Evento { get; set; }
+        public EventoDto? Evento { get; set; }
     }
 }
