f86e114cc1f927f59e39fc1f8328dbeb0ad69499
Weslley Alberto <weslleyalberto@gmail.com>
2025-01-13 17:06:47 -0300

    Adicionada conf no miiddware para Authenticação JWT

--- Back/src/ProEventos.API/ProEventos.API/Program.cs
+++ Back/src/ProEventos.API/ProEventos.API/Program.cs
@@ -1,23 +1,67 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
 using ProEventos.Application;
 using ProEventos.Application.Contratos;
+using ProEventos.Domain.Identity;
 using ProEventos.Persistence;
 using ProEventos.Persistence.Contratos;
+using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ProEventosContext>(options => {
+builder.Services.AddDbContext<ProEventosContext>(options =>
+{
     options.UseSqlite("Data Source=ProEventos.db");
 });
+/* Caso não funcione : builder.Services.AddIdentity<User,Role>*/
+builder.Services.AddIdentityCore<User>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 4;
+}).AddRoles<Role>()
+.AddRoleManager<RoleManager<Role>>()
+.AddSignInManager<SignInManager<User>>()
+.AddRoleValidator<RoleValidator<Role>>()
+.AddEntityFrameworkStores<ProEventosContext>()
+.AddDefaultTokenProviders();
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 builder.Services.AddScoped<IEventoPersist, EventoPersist>();
 builder.Services.AddScoped<IGeralPersist, GeralPersist>();
 builder.Services.AddScoped<IEventoService, EventoService>();
 builder.Services.AddScoped<ILotePersist, LotePersist>();
 builder.Services.AddScoped<ILoteService, LoteService>();
-builder.Services.AddControllers().AddNewtonsoftJson( x=> x.SerializerSettings.ReferenceLoopHandling
-= Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+builder.Services.AddScoped<IUserPersist, UserPersist>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
+//    .AddNewtonsoftJson( x=> x.SerializerSettings.ReferenceLoopHandling
+//= Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddCors();
@@ -43,7 +87,7 @@ app.UseStaticFiles(new StaticFileOptions()
     FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),
     "Resources")),
     RequestPath = new PathString("/Resources")
-}   
+}
 );
 app.MapControllers();
 
